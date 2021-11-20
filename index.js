88da5029d49987f63656035843793c53d7399718
Tonny Leao <Tonnyleao91@gmail.com>
2021-11-20 14:32:24 -0800

    Adding index

--- index.js
+++ index.js
@@ -103,9 +103,14 @@ require('./routes/authRoutes')(app);
 
 // app.use(express.static(__dirname + '/public'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+//   });
+
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
