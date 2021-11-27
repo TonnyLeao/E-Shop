41d118de94661db1ce149b121e88a9073e4bd42c
Tonny Leao <Tonnyleao91@gmail.com>
2021-11-26 20:15:06 -0800

    adding cors

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ require('./routes/authRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     const path = require('path');
-    app.get('*', (req, res) => {
+    app.get('*', cors(), (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
