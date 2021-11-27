e7a3539c3c6001bfad5cec3f35aeb77adaed2cca
Tonny Leao <Tonnyleao91@gmail.com>
2021-11-26 20:54:36 -0800

    adding cors

--- index.js
+++ index.js
@@ -42,7 +42,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors({credentials: true, origin: true}))
+app.use(cors());
+app.options('*', cors());
+
+// app.use(cors({credentials: true, origin: true}))
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
